2c1dd755f67075b9ff72b0a53c16f00ba922eef4
alubadillah <alubaidillah1@gmail.com>
2025-05-24 13:42:51 +0700

    refactor: simplify App component by removing Card component and related logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
-import { useEffect, useState } from 'react'
-import './index.css'
-
-const Card = ({title}) => {
-  const [hasLiked, setHasLiked] = useState(false);
-const [count, setCount] = useState(0)
-
-  useEffect(() =>{
-    console.log(`${title} has been liked: ${hasLiked}`)
-  }, [hasLiked]);
-
-  return(
-    <div className='card' onClick={() => setCount(count + 1)}>
-      <h2>{title} {count ? count : null}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '🤍'}
-      </button>
-    </div>
-  )
-}
+import React from 'react'
 
 function App() {
-
-  
-  
-
   return (
-    <div className='card-container'>
-    <Card title="Star Wars" isCool={true} />
-    <Card title="Avatar" />
-    <Card title="The Lion King" />
-    </div>
+    <div>App</div>
   )
 }
 
