868885a35ec750d4d78c76e3c5fd5cc426420685
alubadillah <alubaidillah1@gmail.com>
2025-05-24 14:26:00 +0700

    Add Search component to display search term

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import Search from './components/Search.jsx'
+import { useState } from 'react'
 
 function App() {
+
+  const [searchTerm, setSearchTerm] = useState('Naruto')
+
   return (
     <main>
-      <div className='patters'></div>  
+      <div className='pattern'></div>  
       
       <div className='wrapper'>
         <header>
+          <img src="./public/hero-img.png" alt="Hero Image" />
           <h1> Find <span className='text-gradient'>Movies</span> You Want Bro!</h1>
         </header>
 
-        <p>Search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
       </div>
         </main>
     
---
+++ src/components/Search.jsx
@@ -0,0 +1,9 @@
+
+const Search = (props) => {
+
+    return(
+        <div className="text-white text-3xl">{props.searchTerm}</div>
+    )
+}
+
+export default Search
