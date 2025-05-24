d6d95ee1ff37f66b0958c360d3047d57c14a3045
alubadillah <alubaidillah1@gmail.com>
2025-05-24 17:51:55 +0700

    feat: implement updateSearchCount function to track and update search queries in the database

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,10 +1,31 @@
-const PPROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+import { Client } from "appwrite";
+
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID)
 
 
+const database = new Databases(client);
 
-export const updateSearchCount = async () => {
-    console.log(PPROJECT_ID, DATABASE_ID, COLLECTION_ID);
+export const updateSearchCount = async (searchTerm, movie) => {
+    try {
+        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.equal( 'searchTerm', searchTerm)])
+
+        if(result.document.length > 0) {
+        const doc = result.document[0];
+
+        await databasse.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {count: doc.count +1,})
+        } else {
+            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {searchTerm, count: 1, movie_id: movie.id, poster_irl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`})
+        } 
+    } catch(error) {
+        console.error(error)
+    }
+
+    
 }
